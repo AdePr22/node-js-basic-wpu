ceca86394a0c8a7745fa981f7b6cd2d1a0d086aa
AdePr22 <Adep.pr88@gmail.com>
2023-03-18 10:34:34 +0700

    Menambahkan folde pertemuan pertama yang diawali dari modul 7 yang membahas tentang node js core module serta membuat contoh penggunaaanya

---
+++ 07-CoreModue/contact/app.js
@@ -0,0 +1,11 @@
+const contact = require(./contact)
+
+const main = async() =>{
+  const nama = await contact.tulisPertanyaan('Masukan Nama anda :')
+  const nama = await contact.tulisPertanyaan('Masukan Nama anda :')
+  const nama = await contact.tulisPertanyaan('Masukan Nama anda :')
+
+  contact.simpanContact(nama,email,noHP)
+}
+ 
+main()
