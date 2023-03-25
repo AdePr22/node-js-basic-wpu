83f4761cd630eece327013b799811e68bde7c4c1
AdePr22 <Adep.pr88@gmail.com>
2023-03-25 15:18:53 +0700

    menambahkan module npm ke dalam aplikasi diantaranya validator,chalk dan nodemon

---
+++ 08-NPM-what/app.js
@@ -0,0 +1,6 @@
+const validator = require("validator");
+const chalk = require("chalk");
+
+// console.log(chalk.blue.validator.isEmail("adep.pr88@gmail.com"));
+console.log(chalk.black.italic.bgBlueBright("hallo kawan"));
+console.log(chalk.black.italic.bgBlueBright("ALhamdulillah"));
